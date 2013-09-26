0921f8589045fa170884507b17abdc1362016373
Moszeed <moszeed@gmail.com>
2013-09-26 09:20:19 +0200

    add precompiled template save
    now the templates will be cahced in it`s precompiled state
    and the promise will be removed if precompiled available

--- backbone-template/README.md
+++ backbone-template/README.md
@@ -1,2 +1,22 @@
 backbone-template
 ================
+optimized plugin to get and set templates to view`s
+
+
+## Dependencies
+- jQuery
+- underscore
+
+## How to use ?
+
+    var view = Backbone.View.extend({
+
+            render : function() {
+
+                this.template({
+                    path    : ' -- Path to Template --',
+                    params  : ' -- Template Params  --',    //optional
+                    success : function() {},                //optional
+                });
+            }
+        });
--- backbone-template/backbone-template.js
+++ backbone-template/backbone-template.js
@@ -1,13 +1,29 @@
 ;(function() {
 
     var Template = {};
-        Template._promises = {};
+        Template._promises  = {};
+        Template._templates = {};
+
 
     function getTemplate(path) {
         var promise = Template._promises[path] || $.get(path);
         Template._promises[path] = promise;
         return promise;
     }
+
+
+    //preload templates
+    Backbone.View.preLoadTemplates = function(templates) {
+
+        if (templates !== void 0) {
+            _(templates).each(function(template) {
+
+                if (Template._templates[template] === void 0) {
+                    getTemplate(template);
+                }
+            });
+        }
+    };
 
     //make template function available in views
     Backbone.View.prototype.template = function(params) {
@@ -21,10 +37,27 @@
         }
 
         var that = this;
+        function displayTpl() {
+            that.$el.html(Template._templates[params.path](params.params));
+            params.success();
+        }
+
+        //use precompiled if available
+        if (Template._templates[params.path] !== void 0) {
+            displayTpl();
+
+            //if promise exist remove
+            if (Template._promises[params.path] !== void 0) {
+                delete Template._promises[params.path];
+            }
+            return true;
+        }
+
+        //load template
         getTemplate(params.path)
             .done(function(tpl) {
-                that.$el.html(_.template(tpl, params.params));
-                params.success();
+                Template._templates[params.path] = _.template(tpl);
+                displayTpl();
             })
             .fail(function() {
                 throw Error('fail to get template');
